1127b6c28635078c1e2644dc9417363ffad35bae
jshbll <ball.joshua@me.com>
2024-02-11 15:46:45 -0500

    add select to median item

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
+document.addEventListener('DOMContentLoaded', function() {
+  // Select all menu items
+  var menuItems = document.querySelectorAll('.menu-item');
+
+  // Calculate the median index
+  var medianIndex = Math.floor(menuItems.length / 2);
+
+  // Remove 'select' class from all items, just in case there's one already set
+  menuItems.forEach(function(item) {
+      item.classList.remove('select');
+  });
+
+  // Add 'select' class to the median item
+  // This also checks if the menuItems collection is not empty
+  if(menuItems.length > 0) {
+      menuItems[medianIndex].classList.add('select');
+  }
+});
+
+
 // Define Functions 
 
 
@@ -32,6 +52,7 @@ function selectPreviousLink() {
     }
   }
 
+  
 // Attach event listeners to all 'trigger' elements
  document.querySelectorAll('.trigger.active.right').forEach(element => {
   element.addEventListener('click', selectNextLink);
